00c645c6345aba621028628aa5f811d0fd95bbb4
DongnutLa <luahernandezdu@unal.edu.co>
2022-03-29 22:23:22 -0500

    [ADD] Complete validation on news form

--- src/components/news/NewForm.jsx
+++ src/components/news/NewForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useContext } from 'react';
+import React, { useRef, useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
 import AuthContext from '@context/AuthContext';
@@ -16,6 +16,9 @@ const NewForm = () => {
   const { headerConfig } = useContext(AuthContext);
   const { types, setOpenToaster } = useContext(ToasterContext);
 
+  const [labels, setLabels] = useState([]);
+  const [error, setError] = useState({})
+  const [btnClass, setBtnClass] = useState('button btn-primary');
   const [filename, setFilename] = useState("Subir archivo");
 
   const form = useRef(null);
@@ -45,20 +48,108 @@ const NewForm = () => {
     }
   }
 
+  const onLabels = (e) => {
+    const validTag = new RegExp(/^[a-z0-9]{3,20}$/);
+    if (e.target.value.includes(' ')) {
+      const value = e.target.value.split(' ', 1).join();
+      if (!validTag.test(value)) {
+        setError({...error, labels: 'La etiqueta debe ser mayor a 2 carácteres y menor a 20'});
+        e.target.value = value;
+      } else {
+        deleteProperty(e.target.name);
+        setLabels([...labels, value]);
+        e.target.value = '';
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (labels.length === 0) setError({...error, labels: 'Este campo es obligatorio*'});
+    if (labels.length > 0) deleteProperty('labels');
+  }, [labels])
+
+  const onBlur = (e) => {
+    if (labels.length === 0 && e.target.value.length === 0) {
+        setError({...error, [e.target.name]: 'Este campo es obligatorio*'});
+    } else {
+      switch (e.target.name) {
+        case 'title':
+          if (e.target.value.length < 10 || e.target.value.length > 50) {
+            setError({...error, title: 'El título debe tener entre 10 y 50 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'content':
+          if (e.target.value.length <= 50) {
+            setError({...error, content: 'El contenido debe ser mayor a 50 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'source':
+          if (e.target.value.length <= 3) {
+            setError({...error, source: 'La fuente debe ser mayor a 3 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  const deleteProperty = (prop) => {
+    const errorState = JSON.parse(JSON.stringify(error));
+    delete errorState[prop];
+    setError(errorState)
+  }
+
+  useEffect(() => {
+    const formData = new FormData(form.current);
+    const title = formData.get('title');
+    const content = formData.get('content');
+    //image: formData.get('image'),
+    const source = formData.get('source');
+    const tags = formData.get('labels');
+    if (Object.keys(error).length > 0 || title.length === 0 || content.length === 0 || source.length === 0) {
+      setBtnClass(btnClass + ' disable');
+    } else {
+      setBtnClass('button btn-primary');
+    }
+  }, [error])
+
+  const onDeleteLabel = (e) => {
+    const values = [...labels];
+    const index = values.indexOf(e.target.textContent)
+    values.splice(index, 1);
+    setLabels(values);
+  }
+
   return (
       <form action="" ref={form}>
         <label htmlFor="title">Título</label>
-        <input type="text" name="title" id="title" placeholder="Título de la noticia"/>
+        <input type="text" name="title" id="title" placeholder="Título de la noticia" onBlur={onBlur}/>
+        {error.title ? <span className='error-msg'>{error.title}</span> : <span className='error-msg'>&nbsp;</span>}
         <label htmlFor="content">contenido</label>
-        <textarea name="content" id="content" cols="30" rows="10" placeholder="Escribe aquí el contenido"></textarea>
+        <textarea name="content" id="content" cols="30" rows="10" placeholder="Escribe aquí el contenido" onBlur={onBlur}></textarea>
+        {error.content ? <span className='error-msg'>{error.content}</span> : <span className='error-msg'>&nbsp;</span>}
         <label htmlFor="image">Imagen</label>
         <label htmlFor="image" className="img-upload button btn-secondary"><img src={upload} alt=""/> <span>{filename}</span></label>
         <input type="file" name="image" id="image" onChange={onFileCharge}/>
         <label htmlFor="source">Fuente</label>
-        <input type="text" name="source" id="source" placeholder="Fuente de la información"/>
+        <input type="text" name="source" id="source" placeholder="Fuente de la información" onBlur={onBlur}/>
+        {error.source ? <span className='error-msg'>{error.source}</span> : <span className='error-msg'>&nbsp;</span>}
         <label htmlFor="labels">Etiquetas</label>
-        <input type="text" name="labels" id="labels" placeholder="Separadas por espacios"/>
-        <button type="button" className="button btn-primary" onClick={handleSubmit}>¡Crear!</button>
+        <input type="text" name="labels" id="labels" placeholder="Separadas por espacios" onChange={onLabels} onBlur={onBlur}/>
+        {error.labels && <span className='error-msg'>{error.labels}</span>}
+        <div className='labels-container'>
+          {labels.map((label, index) => (
+            <span className='labels-name' id={index} key={index} onClick={onDeleteLabel}>{label}</span>
+          ))}
+        </div>
+        <button type="button" disabled={Object.keys(error).length > 0} className={btnClass} onClick={handleSubmit}>¡Crear!</button>
       </form>
   );
 }
