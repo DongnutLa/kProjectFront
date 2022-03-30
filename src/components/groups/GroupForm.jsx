da7cb51cb38944cfae804e6b93d2b3a0f2f63a4f
DongnutLa <luahernandezdu@unal.edu.co>
2022-03-30 06:57:44 -0500

    [ADD] Validation for groups form

--- src/components/groups/GroupForm.jsx
+++ src/components/groups/GroupForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import axios from 'axios';
 
 import AuthContext from '@context/AuthContext';
@@ -11,9 +11,15 @@ const URL = process.env.API;
 const endpoint = 'groups'
 const API = `${URL}${endpoint}`;
 
+const groupTypes = ['Girl group', 'Boy group', 'Mixto'];
+const active = ['Activo', 'Inactivo'];
+
 const GroupForm = () => {
   const { headerConfig } = useContext(AuthContext);
   const { types, setOpenToaster } = useContext(ToasterContext);
+
+  const [error, setError] = useState({})
+  const [btnClass, setBtnClass] = useState('button btn-primary');
   
   const form = useRef(null);
 
@@ -26,60 +32,181 @@ const GroupForm = () => {
       membersNumber: parseInt(formData.get('membersNumber')),
       type: formData.get('type'),
       company: formData.get('company'),
-      fanclubName: formData.get('fanclubName'),
-      fanclubBirth: formData.get('fanclubBirth'),
-      active: formData.get('active') === 'Activo' ? true : false,
-      instagram: formData.get('instagram'),
-      twitter: formData.get('twitter'),
+      active: formData.get('active') === active[0] ? true : false,
     }
+    if (formData.get('fanclubName').length > 0) data.fanclubName = formData.get('fanclubName');
+    if (formData.get('fanclubBirth').length > 0) data.fanclubBirth = formData.get('fanclubBirth');
+    if (formData.get('instagram').length > 0) data.instagram = formData.get('instagram');
+    if (formData.get('twitter').length > 0) data.twitter = formData.get('twitter');
+
     try {
       const res = await axios.post(API, data, headerConfig);
-      setOpenToaster({type: types.SUCCESS, content: 'Grupo creado correctamente'});
+      setOpenToaster({type: types.SUCCESS, content: 'Grupo agregado correctamente'});
     } catch (error) {
-      setOpenToaster({type: types.ERROR, content: 'No se pudo crear el grupo'});
+      setOpenToaster({type: types.ERROR, content: 'No se pudo agregar el grupo'});
     }
   }
+
+  const onBlur = (e) => {
+    const dateValid = new RegExp(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/);
+    const today = parseInt(new Date().toISOString().split('-', 1).join());
+
+    if (e.target.value.length === 0 && e.target.name !== 'instagram' && e.target.name !== 'twitter' && e.target.name !== 'fanclubName' && e.target.name !== 'fanclubBirth') {
+        setError({...error, [e.target.name]: 'Este campo es obligatorio*'});
+    } else {
+      switch (e.target.name) {
+        case 'name':
+          if (e.target.value.length < 3 || e.target.value.length > 20) {
+            setError({...error, name: 'El nombre debe tener entre 3 y 20 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'koreanName':
+          if (e.target.value.length <= 1 || e.target.value.length > 10) {
+            setError({...error, koreanName: 'El nombre debe tener entre 1 y 10 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'debutDate':
+          const year = parseInt(e.target.value.split('-', 1).join());
+          if (!dateValid.test(e.target.value)) {
+            setError({...error, debutDate: 'La fecha no es válida'});
+          } else if (year < 1990 || year > today) {
+            setError({...error, debutDate: `El año debe ser mayor a 1990 y menor a ${today}`});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'membersNumber':
+          if (e.target.value >= 30) {
+            setError({...error, membersNumber: 'Selecciona menos de 30'});
+          } else if (e.target.value < 1) {
+            setError({...error, membersNumber: 'Selecciona al menos 1'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'type':
+          if (!groupTypes.includes(e.target.value)) {
+            setError({...error, type: 'Selecciona un tipo válido'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'company':
+          if (e.target.value.length < 3 || e.target.value.length > 50) {
+            setError({...error, company: 'La empresa debe tener entre 3 y 50 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'fanclubName':
+          if (e.target.value.length < 3 || e.target.value.length > 20) {
+            setError({...error, fanclubName: 'El nombre debe tener entre 3 y 20 carácteres'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'fanclubBirth':
+          let yearFc = parseInt(e.target.value.split('-', 1).join());
+          if (!dateValid.test(e.target.value)) {
+            setError({...error, fanclubBirth: 'La fecha no es válida'});
+          } else if (yearFc < 1990 || yearFc > today) {
+            setError({...error, fanclubBirth: `El año debe ser mayor a 1990 y menor a ${today}`});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        case 'active':
+          if (!active.includes(e.target.value)) {
+            setError({...error, active: 'Selecciona una opción válida'});
+          } else {
+            deleteProperty(e.target.name);
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  const deleteProperty = (prop) => {
+    const errorState = JSON.parse(JSON.stringify(error));
+    delete errorState[prop];
+    setError(errorState)
+  }
+
+  useEffect(() => {
+    const formData = new FormData(form.current);
+    const name = formData.get('name');
+    const koreanName = formData.get('koreanName');
+    const debutDate = formData.get('debutDate');
+    const membersNumber = formData.get('membersNumber');
+    const type = formData.get('type');
+    const company = formData.get('company');
+    //image: formData.get('image'),
+    const active = formData.get('active');
+    if (Object.keys(error).length > 0 || name.length === 0 || koreanName.length === 0 || debutDate.length === 0 || membersNumber.length === 0 || type.length === 0 || company.length === 0 || active.length === 0) {
+      setBtnClass(btnClass + ' disable');
+    } else {
+      setBtnClass('button btn-primary');
+    }
+  }, [error])
 
   return (
     <form action="" ref={form}>
       <label htmlFor="name">Nombre del grupo</label>
-      <input type="text" name="name" id="name"/>
+      <input type="text" name="name" id="name" onBlur={onBlur}/>
+      {error.name ? <span className='error-msg'>{error.name}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="koreanName">Nombre coreano del grupo</label>
-      <input type="text" name="koreanName" id="koreanName"/>
+      <input type="text" name="koreanName" id="koreanName" onBlur={onBlur}/>
+      {error.koreanName ? <span className='error-msg'>{error.koreanName}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="debutDate">Fecha del debut</label>
-      <input type="date" name="debutDate" id="debutDate"/>
+      <input type="date" name="debutDate" id="debutDate" onBlur={onBlur}/>
+      {error.debutDate ? <span className='error-msg'>{error.debutDate}</span> : <span className='error-msg'>&nbsp;</span>}
       <div className="form__divided">
         <div>
           <label htmlFor="membersNumber">Miembros</label>
-          <input type="number" name="membersNumber" id="membersNumber"/>
+          <input type="number" name="membersNumber" id="membersNumber" min={1} max={29} onBlur={onBlur}/>
+          {error.membersNumber ? <span className='error-msg'>{error.membersNumber}</span> : <span className='error-msg'>&nbsp;</span>}
         </div>
         <div>
           <label htmlFor="type">Tipo de grupo</label>
-          <input list="type" name="type" />
+          <input list="type" name="type" onBlur={onBlur}/>
+          {error.type ? <span className='error-msg'>{error.type}</span> : <span className='error-msg'>&nbsp;</span>}
             <datalist id="type">
-              <option value="Girl group"/>
-              <option value="Boy group"/>
-              <option value="Mixto"/>
+              {groupTypes && groupTypes.map(type =>
+                <option key={type} value={type}/>
+              )}
             </datalist>
         </div>
       </div>
       <label htmlFor="company">Empresa</label>
-      <input type="text" name="company" id="company"/>
+      <input type="text" name="company" id="company" onBlur={onBlur}/>
+      {error.company ? <span className='error-msg'>{error.company}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="fanclubName">Nombre del fanclub</label>
-      <input type="text" name="fanclubName" id="fanclubName"/>
+      <input type="text" name="fanclubName" id="fanclubName" onBlur={onBlur}/>
+      {error.fanclubName ? <span className='error-msg'>{error.fanclubName}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="fanclubBirth">Fecha de creación del fanclub</label>
-      <input type="date" name="fanclubBirth" id="fanclubBirth"/>
+      <input type="date" name="fanclubBirth" id="fanclubBirth" onBlur={onBlur}/>
+      {error.fanclubBirth ? <span className='error-msg'>{error.fanclubBirth}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="active">Actividad</label>
-      <input list="active" name="active" />
+      <input list="active" name="active" onBlur={onBlur}/>
+      {error.active ? <span className='error-msg'>{error.active}</span> : <span className='error-msg'>&nbsp;</span>}
         <datalist id="active">
-          <option value="Activo"/>
-          <option value="Inactivo"/>
+          {active && active.map(item =>
+            <option key={item} value={item}/>
+          )}
         </datalist>
       <label htmlFor="instagram">Instagram del grupo</label>
-      <input type="text" name="instagram" id="instagram"/>
+      <input type="text" name="instagram" id="instagram" onBlur={onBlur}/>
+      {error.instagam ? <span className='error-msg'>{error.instagam}</span> : <span className='error-msg'>&nbsp;</span>}
       <label htmlFor="twitter">Twitter del grupo</label>
-      <input type="text" name="twitter" id="twitter"/>
-      <button type="button" className="button btn-primary" onClick={handleSubmit}>Agregar grupo</button>
+      <input type="text" name="twitter" id="twitter" onBlur={onBlur}/>
+      {error.twitter ? <span className='error-msg'>{error.twitter}</span> : <span className='error-msg'>&nbsp;</span>}
+      <button type="button" disabled={Object.keys(error).length > 0} className={btnClass} onClick={handleSubmit}>Agregar grupo</button>
     </form>
   );
 }
